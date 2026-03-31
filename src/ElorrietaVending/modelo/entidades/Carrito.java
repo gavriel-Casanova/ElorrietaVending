f034a6faa22aa98a9c5f2a3ed38e2392839a632d
gavri <gavri@LAPTOP-NF2POREU>
2026-03-31 17:48:23 +0200

    creacion de pojo carrito

--- src/ElorrietaVending/modelo/entidades/Carrito.java
+++ src/ElorrietaVending/modelo/entidades/Carrito.java
@@ -1,5 +1,25 @@
 package ElorrietaVending.modelo.entidades;
 
-public class Carrito {
+import java.util.List;
 
+public class Carrito {
+	private List<Producto> productos = null;
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	@Override
+	public String toString() {
+		return "Carrito [productos=" + productos + "]";
+	}
+
+	public Carrito(List<Producto> productos) {
+		super();
+		this.productos = productos;
+	}
 }
