b5229ab2db80d1405730fba6977b4f922bca097b
casan <casan@maleducada>
2026-04-06 16:13:06 +0200

    agregacion de DAOS

--- src/ElorrietaVending/modelo/DAO/DAOCliente.java
+++ src/ElorrietaVending/modelo/DAO/DAOCliente.java
@@ -1,5 +1,28 @@
 package ElorrietaVending.modelo.DAO;
 
-public class DAOCliente {
+import java.util.ArrayList;
+
+import ElorrietaVending.modelo.entidades.Personas.Cliente;
+import ElorrietaVending.modelo.interfaces.DaoInterfaces;
+
+public class DAOCliente implements DaoInterfaces <Cliente> {
+
+	@Override
+	public ArrayList<Cliente> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Cliente log) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
