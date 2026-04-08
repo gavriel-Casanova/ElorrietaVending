cfb7a1a5a85e00c1138972527d7b38b4c55d53e9
casan <casan@maleducada>
2026-04-08 17:34:14 +0200

    agregacion de lo daos necesarios

--- src/ElorrietaVending/modelo/DAO/DAOProductos.java
+++ src/ElorrietaVending/modelo/DAO/DAOProductos.java
@@ -2,6 +2,7 @@ package ElorrietaVending.modelo.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,7 +12,7 @@ import ElorrietaVending.modelo.entidades.Producto;
 import ElorrietaVending.modelo.interfaces.DaoInterfaces;
 import ElorrietaVending.modelo.ultils.DBUtils;
 
-public class DAOProductos implements DaoInterfaces <Producto>{
+public class DAOProductos implements DaoInterfaces<Producto> {
 
 	@Override
 	public ArrayList<Producto> getAll() {
@@ -43,13 +44,11 @@ public class DAOProductos implements DaoInterfaces <Producto>{
 				String nombre = resultSet.getString("nombre");
 				double precio = resultSet.getDouble("precio");
 				String tipo = resultSet.getString("tipo");
-				
+
 				producto.setId(id);
 				producto.setNombre(nombre);
 				producto.setPrecio(precio);
 				producto.setTipo(tipo);
-				
-				
 
 				ret.add(producto);
 			}
@@ -84,11 +83,11 @@ public class DAOProductos implements DaoInterfaces <Producto>{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public ArrayList<Producto> getByTipo(String tipo) {
 		ArrayList<Producto> ret = null;
 
-		String sql = "select * from producto where tipo = '"+tipo+"'";
+		String sql = "select * from producto where tipo = '" + tipo + "'";
 
 		Connection connection = null;
 		Statement statement = null;
@@ -114,7 +113,7 @@ public class DAOProductos implements DaoInterfaces <Producto>{
 				String nombre = resultSet.getString("nombre");
 				double precio = resultSet.getDouble("precio");
 				String tipo2 = resultSet.getString("tipo");
-				
+
 				producto.setId(id);
 				producto.setNombre(nombre);
 				producto.setPrecio(precio);
@@ -148,11 +147,125 @@ public class DAOProductos implements DaoInterfaces <Producto>{
 		return ret;
 	}
 
-
 	@Override
 	public void insert(Producto log) {
-		// TODO Auto-generated method stub
-		
+		Connection connection = null;
+
+		// Vamos a lanzar una sentencia SQL contra la BBDD
+		Statement statement = null;
+
+		try {
+			// El Driver que vamos a usar
+			Class.forName(DBUtils.DRIVER);
+
+			// Abrimos la conexion con BBDD
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			// Vamos a lanzar la sentencia...
+			statement = connection.createStatement();
+
+			// Montamos la SQL. Esta es una forma simple de hacerlo, hay otra mejor...
+			String sql = "INSERT INTO `producto`( `nombre`, `precio`, `tipo`) VALUES (?,?,?)";
+			PreparedStatement ps = connection.prepareStatement(sql);
+
+			ps.setString(1, log.getNombre());
+			ps.setDouble(2, log.getPrecio());
+			ps.setString(3, log.getTipo());
+
+			ps.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// Cerramos al reves de como las abrimos
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+		}
+
+	}
+
+	public void UpdateProyecto(Producto log) {
+
+		Connection connection = null;
+
+		PreparedStatement preparedStatement = null;
+
+		try {
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			String sql = "UPDATE `producto` SET `nombre`= ?,`precio`= ?,`tipo`= ? WHERE id = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, log.getNombre());
+			preparedStatement.setDouble(2, log.getPrecio());
+			preparedStatement.setString(3, log.getTipo());
+			preparedStatement.setInt(4, log.getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	public void delete(Producto log) {
+		Connection connection = null;
+
+		PreparedStatement preparedStatement = null;
+
+		try {
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			String sql = "DELETE FROM `producto` WHERE id = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, log.getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+		}
 	}
 
 }
--- src/ElorrietaVending/vista/paneles/panelAdministrador/PanelAdministrador.java
+++ src/ElorrietaVending/vista/paneles/panelAdministrador/PanelAdministrador.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 public class PanelAdministrador extends JPanel{
 
